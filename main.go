09b4a334d2b3233a5fc34a6bd3cbf0e538c8f3c9
Wowo10 <wojtekwojtek529@gmail.com>
2025-05-19 22:09:37 +0200

    Added parsing of args.

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	deleteFlag := flag.Bool("delete-missing", false, "Delete Missing files in target directory")
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [options] <src> <target>\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	src := args[0]
+	target := args[1]
+
+	fmt.Println("Source:", src)
+	fmt.Println("Target:", target)
+	fmt.Println("Skip:", *deleteFlag)
 }
